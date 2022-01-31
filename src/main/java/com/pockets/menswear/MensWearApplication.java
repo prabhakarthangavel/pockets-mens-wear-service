e6aeffe8c05b842ddf38c0cd440beb9940efe010
Prabhakar Thangavel <prabhakart.ge@gmail.com>
2022-01-31 19:46:44 +0530

    first-commit

---
+++ src/main/java/com/pockets/menswear/MensWearApplication.java
@@ -0,0 +1,13 @@
+package com.pockets.menswear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensWearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pockets/menswear/MensWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pockets.menswear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
